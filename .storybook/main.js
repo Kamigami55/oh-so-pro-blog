410159fce484d3fdc8ed78a37c7ff12659fff38d
Kamigami55 <eason@easonchang.com>
2021-10-17 15:25:27 +0800

    Add absolute import support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -13,4 +15,12 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src/'),
+    }
+    return config
+  },
 }
